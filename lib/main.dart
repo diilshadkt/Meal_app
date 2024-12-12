753e36fad8376c170c986ae7730594f17b8308f1
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-12 13:53:09 +0530

    create future provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_db_app/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child:  MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ lib/provider/random_meal_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_db_app/models/random_meal_model.dart';
+
+final fetchRandomMealProvider = FutureProvider<Meal?>((ref) async{
+  
+
+  
+});
