8a402952ce70386350bb2fa3886db4f1c8def1ba
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-12 16:30:50 +0530

    home page created

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_db_app/models/random_meal_model.dart';
+import 'package:meal_db_app/provider/random_meal_provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<Meal> randomMealProvider = ref.watch(fetchRandomMealProvider);
     return Scaffold(
       backgroundColor: Colors.amber,
+      body: randomMealProvider.when(
+          data: (data) {
+            return Center(
+              child: Column(
+                children: [
+                  Text(data.strMeal),
+                  Text(data.strCategory),
+                  Text(data.strArea),
+                ],
+              ),
+            );
+          },
+          error: (error, stackTrace) {
+            return Center(
+              child: Text(error.toString()),
+            );
+          },
+          loading:() => Center(child: CircularProgressIndicator(),),),
     );
   }
 }
