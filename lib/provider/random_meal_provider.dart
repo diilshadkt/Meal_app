d521814e592b5313be32b9c4a336f8da92eec23f
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-12 13:59:51 +0530

    url

--- lib/provider/random_meal_provider.dart
+++ lib/provider/random_meal_provider.dart
@@ -1,8 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_db_app/models/random_meal_model.dart';
 
-final fetchRandomMealProvider = FutureProvider<Meal?>((ref) async{
-
-
-  
+final fetchRandomMealProvider = FutureProvider<Meal?>((ref) async {
+  final dio = Dio();
+  final response =
+      await dio.get("https://www.themealdb.com/api/json/v1/1/random.php");
+  return Meal.fromJson(response.data["meals"][0]);
 });
