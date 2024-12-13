01b4854f28e818aeb3158d548fc19c4f5a36b6c3
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-13 14:31:56 +0530

    model class updated

--- lib/provider/random_meal_provider.dart
+++ lib/provider/random_meal_provider.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_db_app/models/random_meal_model.dart';
-final fetchRandomMealProvider = FutureProvider<Meal>((ref) async {
+final fetchRandomMealProvider = FutureProvider<Meal>((ref) async{
   final dio = Dio();
   final response =
       await dio.get("https://www.themealdb.com/api/json/v1/1/random.php");
